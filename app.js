6a1dccd2276f9f09708a889cc4e919d2b38cdd82
Akki-net <akashsharma.zak@gmail.com>
2024-06-02 21:32:10 +0530

    uibuild

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'));
 
 // function requestLogger(request, response, next) {
 //     console.log('path: ', request.path)
